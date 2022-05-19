81193958f59b79ac6e9afa1c43e4fb5ff3e3fd84
Gibran Gonzalez Perez <glzgibran@gmail.com>
2022-05-19 12:17:34 -0500

    ARRAY INGRESOS Y EGRESOS FUNCIONALES pendiente la funcionalidad de saldo inicial y saldo Final

---
+++ db/funciones.php
@@ -0,0 +1,118 @@
+<?php 
+    
+    function ingresosMes($conn){
+        $arrayIngresos = array();   //<- array para almacenar la suma del total de Ingresos por mes
+        for($mes=1; $mes<=12;++$mes){
+            $sqlMesIngreso = "SELECT SUM(cantidad) FROM ingreso_egreso where tipo = 'Ingreso' && MONTH(fecha) = $mes";
+            $tMesIngreso= consultaSuma($conn,$sqlMesIngreso);
+            array_push($arrayIngresos,$tMesIngreso); // <- push para meter los valores
+        }
+        return $arrayIngresos;
+    }
+    function egresosMes($conn){
+        $arrayEgresos = array();    //<- array para almacenar la suma del total de Egresos por mes
+        for($mes=1; $mes<=12;++$mes){
+            $sqlMesEgreso = "SELECT SUM(cantidad) FROM ingreso_egreso where tipo = 'Egreso' && MONTH(fecha) = $mes";
+            $tMesEgreso= consultaSuma($conn,$sqlMesEgreso);
+            array_push($arrayEgresos,$tMesEgreso);  // <- push para meter los valores
+        }
+        return $arrayEgresos;
+    }
+
+    //esta funcion devulve un array con los ingresos y egresos por mes
+    function consultaArray($conn)
+    {   
+        //$sqlMesEgreso = "SELECT * FROM ingreso_egreso where tipo = 'Egreso' && MONTH(fecha) = 3";
+        //Modelo del array multidimensional
+        /*
+        $testeo = [ 
+            0 => array(
+                "Mes" => "Enero",
+                "valores" => array(
+                    "ingreso" => 1.1, 
+                    "egreso" => 2.2)
+                ), 
+            1 => array(
+                "Mes" => "Febrero",
+                "valores" => array(
+                    "ingreso" => 1.1, 
+                    "egreso" => 2.2)
+                ), 
+            2 => array(
+                "Mes" => "Marzo",
+                "valores" => array(
+                    "ingreso" => 1.1, 
+                    "egreso" => 2.2)
+                ), 
+            3 => array(
+                "Mes" => "Abril",
+                "valores" => array(
+                    "ingreso" => 1.1, 
+                    "egreso" => 2.2)
+                ), 
+        ];*/
+        
+        //consulta  de la suma del total de los movimientos por mes Ingresos Y Egresos y guardandolos
+        //en un array cada uno
+        $arrayIngresos = array();   //<- array para almacenar la suma del total de Ingresos por mes
+        $arrayEgresos = array();    //<- array para almacenar la suma del total de Egresos por mes
+        $meses= array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+        $arrayMulti= array();      //<-array donde se guardaran todos los meses del mes con sus ingresos y egresos
+
+        // este for se encaraga de almacenar los valores de la consulta en sus respectivos arrays
+        for($mes=1; $mes<=12;++$mes){
+            $sqlMesIngreso = "SELECT SUM(cantidad) FROM ingreso_egreso where tipo = 'Ingreso' && MONTH(fecha) = $mes";
+            $sqlMesEgreso = "SELECT SUM(cantidad) FROM ingreso_egreso where tipo = 'Egreso' && MONTH(fecha) = $mes";
+            $tMesIngreso= consultaSuma($conn,$sqlMesIngreso);
+            $tMesEgreso= consultaSuma($conn,$sqlMesEgreso);
+            array_push($arrayIngresos,$tMesIngreso); // <- push para meter los valores
+            array_push($arrayEgresos,$tMesEgreso);  // <- push para meter los valores
+        }
+        // este se encarga de crear el objeto multidimensial con los arrays del for anterior
+        for($cont=0; $cont<=11;++$cont){
+            $test = [
+                    'mes' => "$meses[$cont]",
+                    'movimientos' => [
+                        "ingreso" => $arrayIngresos[$cont], 
+                        "egreso" => $arrayEgresos[$cont]
+                    ]
+                ];
+            array_push($arrayMulti,$test);
+        }
+        // ------ IMPORTANTE -----
+        //Si se usa lo siguiente:
+        //####################
+        //$json = json_encode($arrayprueba);
+        //return $json;
+        //####################
+        //Se tendra que usar un json_decode para usar el return en php
+
+        return $arrayMulti ;
+        //return $array;
+    }
+
+    // funcion que realiza una consulta sql y 
+    // devuelve un array
+    function consultaSumTotalMes($conn,$sql)
+    {
+        $result = mysqli_query($conn, $sql);
+        $data = mysqli_fetch_all ($result, MYSQLI_NUM);
+        return $data;
+    }
+
+    // funcion que realiza una consulta de SUM() y 
+    // devuelve un float con 2 decimales
+    function consultaSuma($conn,$sql)
+    {
+        $result = mysqli_query($conn, $sql);
+        $data = mysqli_fetch_row ($result);
+        $valor = round((float) $data[0],2 );
+        return $valor;
+    }
+
+    //funcion para devolver el mes en el cual se esta trabajando
+    function mesActualEsp(){
+        $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+        echo $meses[date('n')-1];
+    }
+?>
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
-<?php 
+<?php
     include 'db/db.php';
-    include 'db/consultas.php';
-
+    include 'db/funciones.php';
     // sentencia de consultas
     $sqlIngreso = "SELECT * FROM  ingreso_egreso where tipo = 'Ingreso'";
     $sqlEgreso = "SELECT * FROM  ingreso_egreso where tipo = 'Egreso'";
@@ -9,38 +8,100 @@
     $sqlSumEgreso = "SELECT SUM(cantidad) FROM  ingreso_egreso where tipo = 'Egreso'";
     $sqlSumIngreso = "SELECT SUM(cantidad) FROM  ingreso_egreso where tipo = 'Ingreso'";
 
-    $sqlMesEgreso = "SELECT * FROM ingreso_egreso where tipo = 'Egreso' && MONTH(fecha) = 3";
-    $sqlMesIngreso = "SELECT * FROM ingreso_egreso where tipo = 'Ingreso' && MONTH(fecha) = 3";
-
-    //consulta de los totales globales
+    //consulta de los totales globales mediante el uso de funciones
     $totalIngresos = consultaSuma($conn,$sqlSumIngreso); //<- Suma del total de Ingresos
     $totalEgresos = consultaSuma($conn,$sqlSumEgreso);   //<- Suma del total de Egresos
+    $MovimientosMes = consultaArray($conn);   //<- Array con la sum de egresos y ingresos por mes
+
     
-    //consulta  de la suma de movimientos por mes
-    $ArrayIngresos = array();   //<- Se alamcenan en un array la suma del total de Ingresos
-    $ArrayEgresos = array();    //<- Se alamcenan en un array la suma del total de Egresos
 
-    echo "Los Ingresos son: $$totalIngresos y los egresos son $$totalEgresos. ";
-    echo '<br>';
+    $i = ingresosMes($conn);
+    $e = egresosMes($conn);
 
-    for($mes=1; $mes<=12;++$mes){
-        $sqlMesIngreso = "SELECT SUM(cantidad) FROM ingreso_egreso where tipo = 'Ingreso' && MONTH(fecha) = $mes";
-        $sqlMesEgreso = "SELECT SUM(cantidad) FROM ingreso_egreso where tipo = 'Egreso' && MONTH(fecha) = $mes";
-        $tMesIngreso= consultaSuma($conn,$sqlMesIngreso);
-        $tMesEgreso= consultaSuma($conn,$sqlMesEgreso);
-        array_push($ArrayIngresos,$tMesIngreso);
-        array_push($ArrayEgresos,$tMesEgreso);
-    }
-      
-    var_dump($ArrayIngresos);
-    echo '<br>';
-    var_dump($ArrayEgresos);  
-    echo '<br>';
-    $Array= array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+    //var_dump($i);
+    //var_dump($e);
 
+    $arrayInicio = array();
+    $arrayFinal = array();
+
+    $fEnero = $i[0]-$e[0];
+    
     for($mes=0; $mes<=11;++$mes){
-        echo " En $Array[$mes] los ingresos son $ArrayIngresos[$mes] y los egresos son $ArrayEgresos[$mes] ";
-        echo '<br>';
+        echo $i[$mes]-$e[$mes];
+        echo "<br>";
     }
-    
 ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <div>
+        <h1>Mes actual: <?php echo mesActualEsp();?></h1>
+        <h2>Debito</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Saldo Inicial</th>
+                    <th>Ingresos</th>
+                    <th>Egresos</th>
+                    <th>Saldo Final</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td> <?php echo $MovimientosMes[date('n')-2]["movimientos"]["ingreso"] - $MovimientosMes[date('n')-2]["movimientos"]["egreso"];?></td>
+                    <td>$ <?php echo $MovimientosMes[date('n')-1]["movimientos"]["ingreso"];?></td>
+                    <td>$ <?php echo $MovimientosMes[date('n')-1]["movimientos"]["egreso"];?></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+   <!-- Mover a otra hoja--> 
+  <h2>Movimientos Tarjeta de debito</h2>
+  <table>
+    <thead>
+      <tr>
+        <th>Mes</th>
+        <th>Ingresos</th>
+        <th>Egresos</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        foreach($MovimientosMes as $array){
+            echo "<tr>" ;
+                echo "<td>";
+                echo  $array["mes"];
+                echo  "</td>";
+                echo "<td>$ ";
+                echo  $array["movimientos"]["ingreso"];
+                echo  "</td>";
+                echo "<td>$ ";
+                echo  $array["movimientos"]["egreso"];
+                echo  "</td>";
+                echo "<td> <button type='button'>Detalles</button></td>";
+            echo "</tr>";
+        }
+        echo "<tr>";
+             echo "<td> Total </td>";
+             echo "<td>$ ";
+             echo $totalIngresos;
+             echo "</td>";
+             echo "<td>$ ";
+             echo $totalEgresos;
+             echo "</td>";
+        echo "</tr>";
+      ?>
+    </tbody>
+  </table> 
+    
+</body>
+</html>
